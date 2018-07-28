181d71dd123c3957913f07a8259c58f67470bde9
daizy <daizy@email.com>
2018-07-28 18:10:37 +0800

    第二次次提交，没有那么害怕啦

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("第一次提交");
          System.out.println("另个人更改的");
+        System.out.println("第二次提交");
+
     }
 }
